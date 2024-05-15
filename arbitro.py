626c6c454210f02b43503890e4c000166de259cb
giovanna.tonelli <giovanna_tonelli@hotmail.com>
2024-05-15 17:57:21 -0300

    reorganizacao por conta do problema de importacao

--- arbitro.py
+++ arbitro.py
@@ -1,4 +1,4 @@
-from pessoa import Pessoa, model
+from pessoa import Pessoa
 
 class Arbitro(Pessoa):
     def __init__(self, nome: str, cpf: str, data_nascimento, num_partidas: int):
@@ -12,4 +12,3 @@ class Arbitro(Pessoa):
     @num_partidas.setter
     def num_partidas(self, num_partidas):
         self.__num_partidas = num_partidas
-    
