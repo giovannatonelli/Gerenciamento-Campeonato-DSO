581db0b2fa0e07ea7426bb5f1109dcaf4d4d4ec9
giovanna.tonelli <giovanna_tonelli@hotmail.com>
2024-05-19 17:01:45 -0300

    organizacao de arquivo

--- controlador_curso.py
+++ controlador_curso.py
@@ -1,7 +1,59 @@
+class ControladorCurso:
+    def __init__(self):
+        self.cursos = []
+        self.tela_curso = TelaCurso()
 
+    def inclui_curso(self):
+        codigo, nome = self.tela_curso.solicita_dados_curso()
+        curso = Curso(codigo, nome)
+        self.cursos.append(curso)
+        self.tela_curso.mostrar_mensagem("Curso adicionado com sucesso!")
 
-class ControladorCurso():
-    def __init__(self) -> None:
-        pass
+    def altera_dados_curso(self):
+        codigo = int(input("Digite o código do curso que deseja alterar: "))
+        curso = self.pega_curso_por_codigo(codigo)
+        if curso:
+            novo_codigo, novo_nome = self.tela_curso.solicita_dados_curso()
+            curso.codigo_curso = novo_codigo
+            curso.nome_curso = novo_nome
+            self.tela_curso.mostrar_mensagem("Dados do curso alterados com sucesso!")
+        else:
+            self.tela_curso.mostrar_mensagem("Curso não encontrado!")
 
-#1. Cada curso precisa ter pelo menos 1 time.
+    def exclui_curso(self):
+        codigo = int(input("Digite o código do curso que deseja excluir: "))
+        curso = self.pega_curso_por_codigo(codigo)
+        if curso:
+            self.cursos.remove(curso)
+            self.tela_curso.mostrar_mensagem("Curso excluído com sucesso!")
+        else:
+            self.tela_curso.mostrar_mensagem("Curso não encontrado!")
+
+    def pega_curso_por_codigo(self, codigo):
+        for curso in self.cursos:
+            if curso.codigo_curso == codigo:
+                return curso
+        return None
+
+    def listar_cursos(self):
+        if self.cursos:
+            for curso in self.cursos:
+                self.tela_curso.mostra_dados_curso(curso)
+        else:
+            self.tela_curso.mostrar_mensagem("Nenhum curso cadastrado!")
+
+    def abre_tela_curso(self):
+        while True:
+            opcao = self.tela_curso.tela_opcoes_curso()
+            if opcao == 1:
+                self.listar_cursos()
+            elif opcao == 2:
+                self.inclui_curso()
+            elif opcao == 3:
+                self.exclui_curso()
+            elif opcao == 4:
+                self.altera_dados_curso()
+            elif opcao == 5:
+                return
+            else:
+                print("Opção inválida!")
--- controlador_equipe.py
+++ controlador_equipe.py
@@ -1,7 +1,111 @@
 
+class ControladorEquipe:
+    def __init__(self):
+        self.equipes = []
+        self.tela_equipe = TelaEquipe()
 
-class ControladorEquipe():
-    def __init__(self) -> None:
+    def inclui_equipe(self):
+        nome = self.tela_equipe.solicita_equipe()
+        equipe = Equipe(nome)
+        self.equipes.append(equipe)
+        self.tela_equipe.mostrar_mensagem("Equipe adicionada com sucesso!")
+
+    def inclui_pontos(self):
+        nome_equipe = self.tela_equipe.solicita_equipe()
+        equipe = self.pega_equipe_por_nome(nome_equipe)
+        if equipe:
+            pontos = int(input("Digite o número de pontos a adicionar: "))
+            equipe.set_num_pontos(equipe.get_num_pontos() + pontos)
+            self.tela_equipe.mostrar_pontos_equipe(equipe)
+        else:
+            self.tela_equipe.mostrar_mensagem("Equipe não encontrada!")
+
+    def altera_dados_equipe(self):
+        nome_equipe = self.tela_equipe.solicita_equipe()
+        equipe = self.pega_equipe_por_nome(nome_equipe)
+        if equipe:
+            novo_nome = input("Digite o novo nome da equipe: ")
+            equipe.set_nome(novo_nome)
+            self.tela_equipe.mostrar_mensagem("Dados da equipe alterados com sucesso!")
+        else:
+            self.tela_equipe.mostrar_mensagem("Equipe não encontrada!")
+
+    def exclui_equipe(self):
+        nome_equipe = self.tela_equipe.solicita_equipe()
+        equipe = self.pega_equipe_por_nome(nome_equipe)
+        if equipe:
+            self.equipes.remove(equipe)
+            self.tela_equipe.mostrar_mensagem("Equipe removida com sucesso!")
+        else:
+            self.tela_equipe.mostrar_mensagem("Equipe não encontrada!")
+
+    def pega_equipe_por_nome(self, nome):
+        for equipe in self.equipes:
+            if equipe.get_nome() == nome:
+                return equipe
+        return None
+
+    def listar_equipes(self):
+        if self.equipes:
+            for equipe in self.equipes:
+                self.tela_equipe.mostra_dados_equipe(equipe)
+        else:
+            self.tela_equipe.mostrar_mensagem("Nenhuma equipe cadastrada!")
+
+    def abre_tela_equipe(self):
+        while True:
+            opcao = self.tela_equipe.tela_opcoes_equipe()
+            if opcao == 1:
+                self.listar_equipes()
+            elif opcao == 2:
+                self.inclui_equipe()
+            elif opcao == 3:
+                self.exclui_equipe()
+            elif opcao == 4:
+                self.altera_dados_equipe()
+            elif opcao == 5:
+                self.adicionar_aluno_equipe()
+            elif opcao == 6:
+                self.remover_aluno_equipe()
+            elif opcao == 7:
+                return
+            else:
+                print("Opção inválida!")
+
+    def equipe_mais_gols(self):
+        # lógica para encontrar a equipe com mais gols
+        equipe_mais_gols = None
+        return equipe_mais_gols
+
+    def equipe_levou_mais_gols(self):
+        #  para encontrar a equipe que levou mais gols
         pass
 
-#2. Em cada time, só podem ser cadasts do mesmo curso.
+    def equipe_menos_gols(self):
+        #para encontrar a equipe com menos gols
+        pass 
+
+    def adicionar_aluno_equipe(self):
+        nome_equipe = self.tela_equipe.solicita_equipe()
+        equipe = self.pega_equipe_por_nome(nome_equipe)
+        if equipe:
+            nome_aluno = input("Digite o nome do aluno: ")
+            aluno = Aluno(nome_aluno, None, None, None)
+            equipe.get_alunos().append(aluno)
+            self.tela_equipe.mostrar_mensagem("Aluno adicionado à equipe!")
+        else:
+            self.tela_equipe.mostrar_mensagem("Equipe não encontrada!")
+
+    def remover_aluno_equipe(self):
+        nome_equipe = self.tela_equipe.solicita_equipe()
+        equipe = self.pega_equipe_por_nome(nome_equipe)
+        if equipe:
+            nome_aluno = input("Digite o nome do aluno a ser removido: ")
+            for i, aluno in enumerate(equipe.get_alunos()):
+                if aluno.nome == nome_aluno:
+                    del equipe.get_alunos()[i]
+                    self.tela_equipe.mostrar_mensagem("Aluno removido da equipe!")
+                    return
+            self.tela_equipe.mostrar_mensagem("Aluno não encontrado na equipe!")
+        else:
+            self.tela_equipe.mostrar_mensagem("Equipe não encontrada!")
